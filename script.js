b541218ef898bddb09f4581e31081a6ed38caa04
rafaelb56 <rafaelbenitez056@gmail.com>
2024-01-30 10:49:06 -0400

    fix: 30% from the part 2 is completed

--- script.js
+++ script.js
@@ -2,10 +2,29 @@ const variables = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice(variables){
   const randomChoice = variables[Math.floor(Math.random() * variables.length)];
+
+  console.log(randomChoice);
 }
 
 getComputerChoice(variables);
 
 function playRound(playerSelection, computerSelection){
+  if(computerSelection == "Paper" && playerSelection == "Rock"){
+    return "You Lose! Paper beats Rock";
 
+  } else if(computerSelection == "Paper" && playerSelection == "Paper"){
+    return "You tied! You chose Paper";
+
+  } else if(computerSelection == "Paper" && playerSelection == "Scissors"){
+    return "You win! Scissors beats Rock"
+
+  } else if(computerSelection == "Paper" && playerSelection == "Rock"){
+    return "You Lose! Paper beats Rock";
+
+  } else if(computerSelection == "Paper" && playerSelection == "Paper"){
+    return "You tied! You chose Paper";
+    
+  } else if(computerSelection == "Paper" && playerSelection == "Scissors"){
+    return "You win! Scissors beats Rock"
+  } else if 
 }
