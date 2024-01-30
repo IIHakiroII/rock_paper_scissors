46b03cc3b0136347c405871a77669ecddf601916
rafaelb56 <rafaelbenitez056@gmail.com>
2024-01-30 10:35:32 -0400

    fix: part 3 added

---
+++ script.js
@@ -0,0 +1,11 @@
+const variables = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice(variables){
+  const randomChoice = variables[Math.floor(Math.random() * variables.length)];
+}
+
+getComputerChoice(variables);
+
+function playRound(playerSelection, computerSelection){
+
+}
